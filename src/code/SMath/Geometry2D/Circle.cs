e11aa79392deb694d9119586838568cb8680fe61
jirikostiha <48102962+jirikostiha@users.noreply.github.com>
2023-01-06 13:25:06 +0100

    circle

---
+++ src/code/SMath/Geometry2D/Circle.cs
@@ -0,0 +1,36 @@
+﻿namespace SMath.Geometry2D
+{
+    public static class Circle
+    {
+        //perimeter / curve / outline
+
+        public static class Region
+        {
+            public static class Area
+            { }
+        }
+
+        public static class Sector
+        {
+            public static class Region
+            {
+                public static class Area
+                { }
+            }
+        }
+
+        public static class Segment
+        {
+            public static class Region
+            {
+                public static class Area
+                { }
+            }
+        }
+
+        public static class Arc
+        { 
+            //length
+        }
+    }
+}
