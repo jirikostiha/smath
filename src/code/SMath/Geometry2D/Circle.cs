1e364e492051468238f78ef5f0653ae1bd66bc4e
Jiri Kostiha <ijkdata@gmail.com>
2023-01-08 20:41:35 +0100

    devel

--- src/code/SMath/Geometry2D/Circle.cs
+++ src/code/SMath/Geometry2D/Circle.cs
@@ -1,21 +1,132 @@
-﻿namespace SMath.Geometry2D
+﻿using System.Numerics;
+
+namespace SMath.Geometry2D
 {
+    /// <summary>
+    /// Circle shape
+    /// </summary>
+    /// <remarks>
+    /// <a href="https://en.wikipedia.org/wiki/Circle">wikipedia</a>
+    /// </remarks>
     public static class Circle
     {
+        public static string SymetryGroup => "O(2)";
+
+        public static class Radius
+        {
+            public static N FromCircumference<N>(N circumference)
+                where N : IFloatingPoint<N>
+                => circumference / (N.CreateChecked(2) * N.Pi);
+            
+            public static N FromArea<N>(N area)
+                where N : IFloatingPoint<N>, IRootFunctions<N>
+                => N.Sqrt(area / N.Pi);
+        }
+
         //perimeter / curve / outline
+        public static class Perimeter 
+        {
+            public static class Lenght
+            {
+                public static N FromRadius<N>(N radius) 
+                    where N : IFloatingPoint<N>
+                    => N.CreateChecked(2) * N.Pi * radius;
+            }
+
+            public static class Includes
+            {
+                public static bool Point<N>((N X, N Y) center, N radius, (N X, N Y) point)
+                    where N : IFloatingPoint<N>
+                    => default;
+            }
+        }
+
+        public static class Arc
+        {
+            public static class Length
+            {
+                public static N FromAngle<N>(N radius, N angle)
+                        where N : IFloatingPoint<N>
+                        => angle * radius;
+            }
+        }
+
+        public static class Chord
+        {
+            public static class Length
+            {
+                public static N FromAngle<N>(N radius, N angle)
+                    where N : IFloatingPoint<N>, ITrigonometricFunctions<N>
+                    => radius * N.CreateChecked(2) * N.Sin(angle / N.CreateChecked(2));
+            }
+        }
 
         public static class Region
         {
             public static class Area
-            { }
+            {
+                public static N FromRadius<N>(N radius)
+                    where N : IFloatingPoint<N>
+                    => N.Pi * radius * radius;
+            }
+
+            public static class Includes
+            {
+                public static bool Point<N>(N radius, (N X, N Y) point)
+                    where N : IFloatingPoint<N>
+                    => default;
+
+                public static bool Point<N>((N X, N Y) center, N radius, (N X, N Y) point)
+                    where N : IFloatingPoint<N>
+                    => default;
+
+                public static bool LineSegment<N>(N radius, (N X, N Y) a, (N X, N Y) b)
+                    where N : IFloatingPoint<N>
+                    => default;
+
+                public static bool LineSegment<N>((N X, N Y) center, N radius, (N X, N Y) a, (N X, N Y) b)
+                    where N : IFloatingPoint<N>
+                    => default;
+
+                public static bool Circle<N>(N radius, (N X, N Y) center2, N radius2)
+                    where N : IFloatingPoint<N>
+                    => default;
+
+                public static bool Circle<N>((N X, N Y) center, N radius, (N X, N Y) center2, N radius2)
+                    where N : IFloatingPoint<N>
+                    => default;
+            }
         }
 
         public static class Sector
         {
+            public static class Perimeter
+            {
+                public static class Lenght
+                {
+                    public static N FromArcAngle<N>(N radius, N arcAngle)
+                        where N : IFloatingPoint<N>
+                        => radius + radius + Arc.Length.FromAngle(radius, arcAngle);
+
+                    public static N FromArcLength<N>(N radius, N arcLenght)
+                        where N : IFloatingPoint<N>
+                        => radius + radius + arcLenght;
+                }
+            }
+
             public static class Region
             {
                 public static class Area
-                { }
+                {
+                    public static N FromArcAngle<N>(N radius, N arcAngle)
+                        where N : IFloatingPoint<N>
+                        => radius * radius * arcAngle / N.CreateChecked(2);
+
+                    public static N FromArcLength<N>(N radius, N length)
+                        where N : IFloatingPoint<N>
+                        => radius * length / N.CreateChecked(2);
+
+                }
             }
         }
 
@@ -28,9 +139,5 @@
             }
         }
 
-        public static class Arc
-        { 
-            //length
-        }
     }
 }
---
+++ src/code/SMath/Geometry3D/Sphere.cs
@@ -0,0 +1,145 @@
+﻿using System.Numerics;
+
+namespace SMath.Geometry3D
+{
+    /// <summary>
+    /// Sphere shape
+    /// </summary>
+    /// <remarks>
+    /// <a href="https://en.wikipedia.org/wiki/Sphere">wikipedia</a>
+    /// </remarks>
+    public static class Sphere
+    {
+        public static string SymetryGroup => "O(3)";
+
+        public static class Radius
+        {
+            public static N FromSurface<N>(N surface)
+                where N : IFloatingPoint<N>
+                => surface / 0;
+
+            public static N FromVolume<N>(N volume)
+                where N : IFloatingPoint<N>, IRootFunctions<N>
+                => volume;
+        }
+
+        public static class Surface
+        {
+            public static class Area
+            {
+                public static N FromRadius<N>(N radius)
+                    where N : IFloatingPoint<N>
+                    => 0;
+            }
+
+            public static class Includes
+            {
+                public static bool Point<N>((N X, N Y) center, N radius, (N X, N Y, N Z) point)
+                    where N : IFloatingPoint<N>
+                    => default;
+            }
+        }
+
+        public static class Region
+        {
+            public static class Volume
+            {
+                public static N FromRadius<N>(N radius)
+                    where N : IFloatingPoint<N>
+                    => N.Pi * radius * radius;
+            }
+
+            public static class Includes
+            {
+                public static bool Point<N>(N radius, (N X, N Y, N Z) point)
+                    where N : IFloatingPoint<N>
+                    => default;
+
+                public static bool Point<N>((N X, N Y, N Z) center, N radius, (N X, N Y, N Z) point)
+                    where N : IFloatingPoint<N>
+                    => default;
+
+                public static bool LineSegment<N>(N radius, (N X, N Y, N Z) a, (N X, N Y, N Z) b)
+                    where N : IFloatingPoint<N>
+                    => default;
+
+                public static bool LineSegment<N>((N X, N Y, N Z) center, N radius, (N X, N Y, N Z) a, (N X, N Y, N Z) b)
+                    where N : IFloatingPoint<N>
+                    => default;
+
+                public static bool Sphere<N>(N radius, (N X, N Y, N Z) center2, N radius2)
+                    where N : IFloatingPoint<N>
+                    => default;
+
+                public static bool Sphere<N>((N X, N Y, N Z) center, N radius, (N X, N Y, N Z) center2, N radius2)
+                    where N : IFloatingPoint<N>
+                    => default;
+            }
+        }
+
+        /// <summary>
+        /// Spherical cap of a sphere.
+        /// </summary>
+        /// <remarks>
+        /// <a href="https://en.wikipedia.org/wiki/Spherical_cap">wikipedia</a>
+        /// <a href="https://mathworld.wolfram.com/SphericalCap.html">wolfram</a>
+        /// <a href="https://www.cuemath.com/measurement/volume-of-a-section-of-a-sphere/">cuemath</a>
+        /// <a href="https://www.123calculus.com/en/spherical-cap-page-7-40-135.html">calculator 1</a>
+        /// <a href="https://www.redcrab-software.com/en/Calculator/Spherical-Cap">calculator 2</a>
+        /// </remarks>
+        public static class Cap
+        {
+            public static class Surface
+            {
+                public static class Area
+                {
+                    public static N From<N>(N radius)
+                        where N : IFloatingPoint<N>
+                        => 0;
+                }
+
+            }
+
+            public static class Region
+            {
+                public static class Volume
+                {
+                    public static N From<N>(N radius)
+                        where N : IFloatingPoint<N>
+                        => 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Spherical segment
+        /// </summary>
+        /// <remarks>
+        /// <a href="https://en.wikipedia.org/wiki/Spherical_segment">wikipedia</a>
+        /// </remarks>
+
+        public static class Segment
+        {
+            public static class Surface
+            {
+                public static class Area
+                {
+                    public static N From<N>(N radius)
+                        where N : IFloatingPoint<N>
+                        => 0;
+                }
+
+            }
+
+            public static class Region
+            {
+                public static class Volume
+                {
+                    public static N From<N>(N radius)
+                        where N : IFloatingPoint<N>
+                        => 0;
+                }
+            }
+        }
+    }
+}
