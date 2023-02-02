8fa193c5fc94adb7f1f25cfb2e62efa3ef542156
jirikostiha <48102962+jirikostiha@users.noreply.github.com>
2023-02-02 13:50:56 +0000

    sin

---
+++ src/code/SMath/Functions1/Sin.cs
@@ -0,0 +1,55 @@
+﻿using System.Numerics;
+
+namespace SMath.Functions1
+{
+    /// <summary>
+    /// Square function.
+    /// </summary>
+    /// <remarks>
+    /// <a href="https://en.wikipedia.org/wiki/Square_(algebra)">wikipedia</a>
+    /// <a href="https://en.wikipedia.org/wiki/Exponentiation#Power_functions">wikipedia</a>
+    /// </remarks>
+    public class Sin : IMathFunction
+    {        
+        /// <inheritdoc />
+        public static bool IsEven 
+            => true;
+
+        /// <inheritdoc />
+        public static bool IsOdd 
+            => false;
+
+        /// <inheritdoc />
+        public static bool IsContinuous
+            => true;
+
+        /// <inheritdoc />
+        public static string PlainTextFormula
+            => "sin(x)";
+
+        /// <inheritdoc />
+        public static (N Min, N Max) Domain<N>()
+            where N : IFloatingPointIeee754<N>
+            => (N.NegativeInfinity, N.PositiveInfinity);
+
+        /// <inheritdoc />
+        public static (N Min, N Max) NumberDomain<N>()
+            where N : INumberBase<N>, IMinMaxValue<N>
+            => (N.MaxValue, N.MaxValue);
+
+        /// <inheritdoc />
+        public static (N Min, N Max) Image<N>()
+            where N : IFloatingPointIeee754<N>
+            => (N.Zero, N.PositiveInfinity);
+
+        /// <inheritdoc />
+        public static (N Min, N Max) NumberImage<N>()
+            where N : INumberBase<N>, IMinMaxValue<N>
+            => (N.Zero, N.MaxValue);
+
+        /// <inheritdoc />
+        public static N Eval<N>(N x) 
+            where N : IFloatingPointIeee754<N>
+            => N.Sin(x);
+    }
+}
