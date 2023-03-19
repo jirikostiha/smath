6c023adbc7ee7f2dc5126d6d3b230428c200a3f8
Jiri Kostiha <ijkdata@gmail.com>
2023-03-19 15:10:59 +0100

    feat: circle radius, perimeter length

---
+++ src/code/SMath/Geometry2D/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System.Numerics;
+
+namespace SMath.Geometry2D
+{
+    /// <summary>
+    /// Circle.
+    /// </summary>
+    /// <remarks>
+    /// <a href="https://en.wikipedia.org/wiki/Circle">wikipedia</a>
+    /// <a href="https://mathworld.wolfram.com/Circle.html">mathworld</a>
+    /// </remarks>
+    public static class Circle
+    {
+        public static string SymetryGroup => "O(2)";
+
+        /// <summary>
+        /// Radius of a circle.
+        /// </summary>
+        public static class Radius
+        {
+            public static N FromCircumference<N>(N circumference)
+                where N : IFloatingPoint<N>
+                => circumference / (N.CreateChecked(2) * N.Pi);
+            
+            public static N FromArea<N>(N area)
+                where N : IFloatingPoint<N>, IRootFunctions<N>
+                => N.Sqrt(area / N.Pi);
+        }
+
+        /// <summary>
+        /// Perimeter or curve or outline of a circle.
+        /// </summary>
+        public static class Perimeter
+        {
+            /// <summary>
+            /// Circumference of a circle.
+            /// </summary>
+            public static class Length
+            {
+                public static N FromRadius<N>(N radius)
+                    where N : IFloatingPoint<N>
+                    => N.CreateChecked(2) * N.Pi * N.CreateChecked(radius);
+            }
+        }
+    }
+}
---
+++ src/qualiity/Geometry2D/CircleTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using static System.Math;
+
+namespace SMath.Geometry2D
+{
+    public class CircleTests
+    {
+        [Fact]
+        public void Circumference()
+        {
+            Assert.Equal(2 * PI, Circle.Perimeter.Length.FromRadius(1d));
+        }
+
+        [Fact]
+        public void Radius()
+        {
+            Assert.Equal(1d, Circle.Radius.FromCircumference(2 * PI));
+            Assert.Equal(1d, Circle.Radius.FromArea(PI));
+        }
+    }
+}
