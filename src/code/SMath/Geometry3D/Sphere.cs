7e0fccee25f469fb07830515d2bfb278d9595cd7
Jiri Kostiha <ijkdata@gmail.com>
2023-03-19 19:41:47 +0100

    feat: sphere radius, surface area, volume

---
+++ src/code/SMath/Geometry3D/Sphere.cs
@@ -0,0 +1,64 @@
+﻿using System.Numerics;
+
+namespace SMath.Geometry3D
+{
+    /// <summary>
+    /// Sphere.
+    /// </summary>
+    /// <remarks>
+    /// <a href="https://en.wikipedia.org/wiki/Sphere">wikipedia</a>
+    /// <a href="https://mathworld.wolfram.com/Sphere.html">mathworld</a>
+    /// </remarks>
+    public static class Sphere
+    {
+        public static string SymetryGroup => "O(3)";
+
+        public static int EulerCharacteristic => 2;
+
+        /// <summary>
+        /// Radius of a sphere.
+        /// </summary>
+        public static class Radius
+        {
+            public static N FromSurfaceArea<N>(N surface)
+                where N : IFloatingPoint<N>, IRootFunctions<N>
+                => N.Sqrt(surface / (N.CreateChecked(4) * N.Pi));
+
+            public static N FromVolume<N>(N volume)
+                where N : IFloatingPoint<N>, IRootFunctions<N>
+                => N.Cbrt(volume / (N.CreateChecked(4) / N.CreateChecked(3) * N.Pi));
+        }
+
+        /// <summary>
+        /// Surface of a sphere.
+        /// </summary>
+        public static class Surface
+        {
+            /// <summary>
+            /// Surface area of a sphere.
+            /// </summary>
+            public static class Area
+            {
+                public static N FromRadius<N>(N radius)
+                    where N : IFloatingPoint<N>
+                    => N.CreateChecked(4) * N.Pi * radius * radius;
+            }
+        }
+
+        /// <summary>
+        /// Ball region enclosed by a sphere.
+        /// </summary>
+        public static class Region
+        {
+            /// <summary>
+            /// Enclosed volume of a sphere.
+            /// </summary>
+            public static class Volume
+            {
+                public static N FromRadius<N>(N radius)
+                    where N : IFloatingPoint<N>
+                    => N.CreateChecked(4) / N.CreateChecked(3) * N.Pi * radius * radius * radius;
+            }
+        }
+    }
+}
---
+++ src/quality/Geometry3D/SphereTests.cs
@@ -0,0 +1,28 @@
+﻿using SMath.Geometry3D;
+using Xunit;
+using static System.Math;
+
+namespace SMath.Geometry2D
+{
+    public class SphereTests
+    {
+        [Fact]
+        public void SurfaceArea()
+        {
+            Assert.Equal(4d * PI, Sphere.Surface.Area.FromRadius(1d));
+        }
+
+        [Fact]
+        public void Volume()
+        {
+            Assert.Equal(4d/3d * PI, Sphere.Region.Volume.FromRadius(1d));
+        }
+
+        [Fact]
+        public void Radius()
+        {
+            Assert.Equal(1d, Sphere.Radius.FromSurfaceArea(4 * PI));
+            Assert.Equal(1d, Sphere.Radius.FromVolume(4d/3d * PI));
+        }
+    }
+}
