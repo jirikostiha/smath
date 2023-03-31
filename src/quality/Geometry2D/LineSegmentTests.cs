a27c8d7c4d987ee8893c5abc094c6178b20a70e2
C# linter <lint-action@samuelmeuli.com>
2023-03-31 15:11:28 +0000

    style: code style issues with dotnet_format

--- src/quality/Geometry2D/LineSegmentTests.cs
+++ src/quality/Geometry2D/LineSegmentTests.cs
@@ -20,21 +20,24 @@ namespace SMath.Geometry2D
         public void Points()
         {
             Assert.Empty(Line.Segment.Points.Get((0d, 0d), (1d, 1d), 0));
-            
+
             Assert.Collection(Line.Segment.Points.Get((0d, 0d), (1d, 1d), 1),
-                item => { 
+                item =>
+                {
                     Assert.Equal((0.5), item.X, 6);
                     Assert.Equal((0.5), item.Y, 6);
                 });
 
             Assert.Collection(Line.Segment.Points.Get((0d, 0d), (1d, 1d), 2),
-                item => {
-                    Assert.Equal((1/3d), item.X, 6);
-                    Assert.Equal((1/3d), item.Y, 6);
+                item =>
+                {
+                    Assert.Equal((1 / 3d), item.X, 6);
+                    Assert.Equal((1 / 3d), item.Y, 6);
                 },
-                item => {
-                    Assert.Equal((2/3d), item.X, 6);
-                    Assert.Equal((2/3d), item.Y, 6);
+                item =>
+                {
+                    Assert.Equal((2 / 3d), item.X, 6);
+                    Assert.Equal((2 / 3d), item.Y, 6);
                 });
         }
     }
