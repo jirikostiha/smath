62e048577538163abe73568413f4d05d4063bc1f
Jiri Kostiha <ijkdata@gmail.com>
2023-03-31 17:10:04 +0200

    test(geometry): line segment tests refactoring

--- src/quality/Geometry2D/LineSegmentTests.cs
+++ src/quality/Geometry2D/LineSegmentTests.cs
@@ -1,23 +1,41 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using Xunit;
 
 namespace SMath.Geometry2D
 {
     public class LineSegmentTests
     {
-        [Fact]
-        public void Slope()
+        [Theory]
+        [InlineData(0, 0, 1, 0, 0)]
+        [InlineData(0, 0, 0, 1, double.PositiveInfinity)]
+        [InlineData(0, 0, -1, 0, 0)]
+        [InlineData(0, 0, 0, -1, double.NegativeInfinity)]
+        [InlineData(0, 0, 1, 1, 1)]
+        public void Slope(double p1x, double p1y, double p2x, double p2y, double slope)
         {
-            Assert.Equal(0, Line.Segment.Slope.Get((0d, 0d), (1d, 0d)));
-            Assert.Equal(1, Line.Segment.Slope.Get((0d, 0d), (1d, 1d)));
-            //Assert.Equal(not-defined nebo infinity?, Line.Slope.FromTwoPoints((0d, 0d), (1d, 0d)));
+            Assert.Equal(slope, Line.Segment.Slope.Get((p1x, p1y), (p2x, p2y)));
         }
 
         [Fact]
         public void Points()
         {
-            Assert.Equal(0, Line.Segment.Points.Get((0d, 0d), (1d, 1d), 4));
-            Assert.Equal(1, Line.Segment.Points.Get((0d, 0d), (1d, 1d), 1));
-            //Assert.Equal(not-defined nebo infinity?, Line.Slope.FromTwoPoints((0d, 0d), (1d, 0d)));
+            Assert.Empty(Line.Segment.Points.Get((0d, 0d), (1d, 1d), 0));
+            
+            Assert.Collection(Line.Segment.Points.Get((0d, 0d), (1d, 1d), 1),
+                item => { 
+                    Assert.Equal((0.5), item.X, 6);
+                    Assert.Equal((0.5), item.Y, 6);
+                });
+
+            Assert.Collection(Line.Segment.Points.Get((0d, 0d), (1d, 1d), 2),
+                item => {
+                    Assert.Equal((1/3d), item.X, 6);
+                    Assert.Equal((1/3d), item.Y, 6);
+                },
+                item => {
+                    Assert.Equal((2/3d), item.X, 6);
+                    Assert.Equal((2/3d), item.Y, 6);
+                });
         }
     }
 }
