3bbcf6816499f7dfeefacf5e500d8b13d3aedb16
Jiri Kostiha <ijkdata@gmail.com>
2023-04-05 17:58:49 +0200

    test(funct1): identity test fix

--- src/quality/SMath__Tests/Functions1/IdentityTests.cs
+++ src/quality/SMath__Tests/Functions1/IdentityTests.cs
@@ -17,17 +17,17 @@ namespace SMath.Functions1
         [InlineData(-1, -1, 1, 0)]
         public void TangentLineInPoint(double x, double a, double b, double c)
         {
-            var tl = Identity.TangentLine.FromX(x);
-            Assert.Equal(a, tl.A, 6);
-            Assert.Equal(b, tl.B, 6);
-            Assert.Equal(c, tl.C, 6);
+            var line = Identity.TangentLine.FromX(x);
+            Assert.Equal(a, line.A, 6);
+            Assert.Equal(b, line.B, 6);
+            Assert.Equal(c, line.C, 6);
         }
 
         [Fact]
         public void NormalLineSlopeInPoint()
         {
-            // always -1
-            Assert.Equal(-1d, Identity.TangentLine.Slope.FromX<double>(), 6);
+            // always 1
+            Assert.Equal(1d, Identity.TangentLine.Slope.FromX<double>(), 6);
         }
 
         [Theory]
@@ -36,10 +36,10 @@ namespace SMath.Functions1
         [InlineData(-1, 1, 1, 1)]
         public void NormalLineInPoint(double x, double a, double b, double c)
         {
-            var tl = Identity.NormalLine.FromX(x);
-            Assert.Equal(a, tl.A, 6);
-            Assert.Equal(b, tl.B, 6);
-            Assert.Equal(c, tl.C, 6);
+            var line = Identity.NormalLine.FromX(x);
+            Assert.Equal(a, line.A, 6);
+            Assert.Equal(b, line.B, 6);
+            Assert.Equal(c, line.C, 6);
         }
     }
 }
