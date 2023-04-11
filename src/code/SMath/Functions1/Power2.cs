3a80500ad4cf49bb9fd685da709322189f897fa5
Jiri Kostiha <ijkdata@gmail.com>
2023-04-11 20:31:23 +0200

    feat(geometry): parabola

--- src/code/SMath/Functions1/Power2.cs
+++ src/code/SMath/Functions1/Power2.cs
@@ -63,43 +63,5 @@ namespace SMath.Functions1
         public static N DerivativeEval<N>(N x)
             where N : INumberBase<N>
             => N.CreateChecked(2) * x;
-
-        public static class TangentLine
-        {
-            public static (N A, N B, N C) FromX<N>(N x)
-                where N : INumberBase<N>
-            {
-                var slope = Slope.FromX(x);
-                return (-slope, N.One, slope * x - Eval(x));
-            }
-
-            public static class Slope
-            {
-                public static N FromX<N>(N x)
-                    where N : INumberBase<N>
-                    => DerivativeEval(x);
-            }
-        }
-        public static class NormalLine
-        {
-            public static (N A, N B, N C) FromX<N>(N x)
-                where N : INumberBase<N>
-            {
-                if (x != N.Zero)
-                {
-                    var slope = Slope.FromX(x);
-                    return (-slope, N.One, slope * x - Eval(x));
-                }
-                else
-                    return (N.One, N.Zero, N.Zero);
-            }
-
-            public static class Slope
-            {
-                public static N FromX<N>(N x)
-                    where N : INumberBase<N>
-                    => -N.One / DerivativeEval(x);
-            }
-        }
     }
 }
---
+++ src/code/SMath/Geometry2D/Parabola.cs
@@ -0,0 +1,57 @@
+﻿using System.Numerics;
+
+namespace SMath.Geometry2D
+{
+    /// <summary>
+    /// Parabola.
+    /// </summary>
+    /// <remarks>
+    /// <a href="https://en.wikipedia.org/wiki/Parabola">wikipedia</a>
+    /// <a href="https://mathworld.wolfram.com/Parabola.html">mathworld</a>
+    /// </remarks>
+    public static class Parabola
+    {
+        public static class Focus
+        {
+
+        }
+
+        public static class TangentLine
+        {
+            public static (N A, N B, N C) FromX<N>(N x)
+                where N : INumberBase<N>
+            {
+                var slope = Slope.FromX(x);
+                return (-slope, N.One, slope * x - Eval(x));
+            }
+
+            public static class Slope
+            {
+                public static N FromX<N>(N x)
+                    where N : INumberBase<N>
+                    => DerivativeEval(x);
+            }
+        }
+        public static class NormalLine
+        {
+            public static (N A, N B, N C) FromX<N>(N x)
+                where N : INumberBase<N>
+            {
+                if (x != N.Zero)
+                {
+                    var slope = Slope.FromX(x);
+                    return (-slope, N.One, slope * x - Eval(x));
+                }
+                else
+                    return (N.One, N.Zero, N.Zero);
+            }
+
+            public static class Slope
+            {
+                public static N FromX<N>(N x)
+                    where N : INumberBase<N>
+                    => -N.One / DerivativeEval(x);
+            }
+        }
+    }
+}
