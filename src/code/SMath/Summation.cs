e79fb877e2625bffbcd44ce87222443bfc9f5067
Jiri Kostiha <ijkdata@gmail.com>
2023-04-17 10:31:49 +0200

    feat: Summation

---
+++ src/code/SMath/Summation.cs
@@ -0,0 +1,38 @@
+using System.Numerics;
+
+namespace SMath
+{
+    /// <summary>
+    /// Summation.
+    /// </summary>
+    /// <remarks>
+    /// <a href="https://en.wikipedia.org/wiki/Summation">wikipedia</a>
+    /// </remarks>
+    public static class Summation
+    {
+        public static N Eval<N>(IEnumerable<N> summands)
+             where N : INumberBase<N>
+        {
+            var sum = N.Zero;
+            foreach (var summand in summands)
+                sum += summand;
+
+            return sum;
+        }
+
+        public static N Eval<N, NInt>(IEnumerable<N> summands, out NInt count)
+            where N : INumberBase<N>
+            where NInt : IBinaryInteger<NInt>
+        {
+            var sum = N.Zero;
+            count = NInt.Zero;
+            foreach (var summand in summands)
+            {
+                sum += summand;
+                count++;
+            }
+
+            return sum;
+        }
+    }
+}
