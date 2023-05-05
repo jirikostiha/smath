1457c34489746fde3fb1b7bb5c7b75bdebb64b86
Jiri Kostiha <ijkdata@gmail.com>
2023-05-06 00:38:52 +0200

    test(geometry): refactor LineAndPointTests

--- src/quality/SMath__Tests/Geometry2D/LineAndPointTests.cs
+++ src/quality/SMath__Tests/Geometry2D/LineAndPointTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace SMath.Geometry2D
@@ -33,23 +36,41 @@ namespace SMath.Geometry2D
         }
 
         [Theory]
-        [InlineData(-1, 0, 1, 0, -1, 0, -1, 0)] // identical to first point on x-axis
-        [InlineData(-1, 0, 1, 0, 1, 0, 1, 0)]   // identical to second point on x-axis
-        [InlineData(-1, 0, 1, 0, 0, 0, 0, 0)]   // on line (x-axis)
-        [InlineData(-1, 0, 1, 0, -1, 1, -1, 0)] // above first point on x-axis
-        [InlineData(-1, 0, 1, 0, 0, 1, 0, 0)]   // above the center point (origin) on x-axis
-        [InlineData(-1, 0, 1, 0, 1, 1, 1, 0)]   // above second point on x-axis
-        [InlineData(-1, 0, 1, 0, -2, 0, -2, 0)] // inline before first point
-        [InlineData(-1, 0, 1, 0, 2, 0, 2, 0)]   // inline after second point
-        [InlineData(0, 0, 0, 2, 1, 1, 0, 1)]    // right to the first point on y-axis
-        [InlineData(0, 2, 2, 0, 0, 0, 1, 1)]
-        public void Projection_FromPoints(double ax, double ay, double bx, double by,
-          double px, double py, double pointX, double pointY)
+        [MemberData(nameof(TestData.ProjectionData), MemberType = typeof(TestData))]
+        public void Projection_FromPoints(((double X, double Y) P1, (double X, double Y) P2) segment,
+            (double X, double Y) point, (double X, double Y) projectedPoint, string message)
         {
-            var point = Line.And.Point.Projection.FromPoints((ax, ay), (bx, by), (px, py));
+            var evaluatetPoint = Line.And.Point.Projection.FromPoints(segment.P1, segment.P2, point);
 
-            Assert.Equal(pointX, point.X, 6);
-            Assert.Equal(pointY, point.Y, 6);
+            Assert.Equal(projectedPoint.X, evaluatetPoint.X, 6);
+            Assert.Equal(projectedPoint.Y, evaluatetPoint.Y, 6);
+        }
+
+        private static class TestData
+        {
+            public static IEnumerable<object[]> Projection()
+            {
+                yield return new object[] { (-1d, 0d), (1d, 0d), (-1d, 0d), (-1d, 0d), "Identical to first point on x-axis" };
+                yield return new object[] { (-1d, 0d), (1d, 0d), (1d, 0d), (1d, 0d), "identical to second point on x-axis" };
+                yield return new object[] { (-1d, 0d), (1d, 0d), (0d, 0d), (0d, 0d), "on line (x-axis)" };
+                yield return new object[] { (-1d, 0d), (1d, 0d), (-1d, 1d), (-1d, 0d), "above first point on x-axis" };
+                yield return new object[] { (-1d, 0d), (1d, 0d), (0d, 1d), (0d, 0d), "above the center point (origin) on x-axis" };
+                yield return new object[] { (-1d, 0d), (1d, 0d), (1d, 1d), (1d, 0d), "above second point on x-axis" };
+                yield return new object[] { (-1d, 0d), (1d, 0d), (-2d, 0d), (-2d, 0d), "inline before first point" };
+                yield return new object[] { (-1d, 0d), (1d, 0d), (2d, 0d), (2d, 0d), "inline after second point" };
+                yield return new object[] { (0d, 0d), (0d, 2d), (1d, 1d), (0d, 1d), "right to the first point on y-axis" };
+                yield return new object[] { (0d, 2d), (2d, 0d), (0d, 0d), (1d, 1d), "" };
+            }
+
+            public static TheoryData<
+                ((double X, double Y) P1, (double X, double Y) P2),
+                (double X, double Y), (double X, double Y),
+                string> ProjectionData => new()
+            {
+                { ((-1d, 0d), (1d, 0d)), (-1d, 0d), (-1d, 0d), "Identical to first point on x-axis" },
+                //(-1d, 0d), (1d, 0d), (-1d, 0d), (-1d, 0d), "Identical to first point on x-axis" };
+                //(-1d, 0d), (1d, 0d), (1d, 0d), (1d, 0d), "identical to second point on x-axis" };
+            };
         }
     }
 }
