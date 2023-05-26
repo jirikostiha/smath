9d6ead39295e5bf6a06ab259468140798fe7cea4
jirikostiha <48102962+jirikostiha@users.noreply.github.com>
2023-05-26 15:57:54 +0000

    ext

---
+++ src/code/SMath/PythagorasTheorem - Copy.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace SMath
+{
+    /// <summary>
+    /// Geometric distance.
+    /// <a href="">wikipedia</a>
+    /// </summary>
+    public static class Distance  //or point - distance
+    {
+        public static N Hypotenuse<N>(N leg1, N leg2)
+            where N : IRootFunctions<N>
+            => N.Sqrt((leg1 * leg1) + (leg2 * leg2));
+    }
+}
