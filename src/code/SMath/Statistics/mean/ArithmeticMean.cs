bbcafcabac1a4c1b1c3341008c888df8322c0910
Jiri Kostiha <ijkdata@gmail.com>
2023-08-20 16:14:33 +0200

    feat(stats): ArithmeticMean

---
+++ src/code/SMath/Statistics/mean/ArithmeticMean.cs
@@ -0,0 +1,21 @@
+namespace SMath.Statistics
+{
+    using System.Collections.Generic;
+    using System.Numerics;
+
+    /// <summary>
+    /// Arithmetic mean.
+    /// </summary>
+    /// <remarks>
+    /// <a href="https://en.wikipedia.org/wiki/Arithmetic_mean">wikipedia</a>
+    /// </remarks>
+    public static class ArithmeticMean
+    {
+        public static double Eval<N>(IEnumerable<N> sequence)
+            where N : INumberBase<N>
+        {
+            var sum = Summation.Eval(sequence, out long count);
+            return double.CreateChecked(sum) / double.CreateChecked(count);
+        }
+    }
+}
