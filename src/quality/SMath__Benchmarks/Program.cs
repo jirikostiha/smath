8ee27a7a346e46347c0b4ef58343c573b7e76c8e
Jiri Kostiha <ijkdata@gmail.com>
2023-10-26 09:33:59 +0200

    fix(bench): workaround for benchmarks working dir

--- src/quality/SMath__Benchmarks/Program.cs
+++ src/quality/SMath__Benchmarks/Program.cs
@@ -1,17 +1,25 @@
 ﻿using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
+using System.Runtime.CompilerServices;
 
 namespace SMath;
 
 public static class Program
 {
+    private static string GetSourceFilePathName([CallerFilePath] string? callerFilePath = null) => callerFilePath ?? string.Empty;
+
     public static void Main(string[] args)
     {
-        var outputFolder = Path.GetFullPath(Directory.GetCurrentDirectory().EndsWith(typeof(Program).Assembly.GetName().Name ?? string.Empty)
-            ? @"./../../../benchmarks"
-            : @"./../../../../../../benchmarks");
+        Console.WriteLine("directory: " + Directory.GetCurrentDirectory());
 
-        ManualConfig config = ManualConfig.Create(DefaultConfig.Instance.WithArtifactsPath(outputFolder))
+        if (!Directory.GetCurrentDirectory().EndsWith(typeof(Program).Assembly.GetName().Name ?? string.Empty))
+        {
+            //HACK: workaround for executing from VS because the output path of binaries is in different folder than usual
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(GetSourceFilePathName()) ?? string.Empty);
+            Console.WriteLine("changed to: " + Directory.GetCurrentDirectory());
+        }
+
+        ManualConfig config = ManualConfig.Create(DefaultConfig.Instance.WithArtifactsPath("./../../../benchmarks"))
             .WithOption(ConfigOptions.JoinSummary, true)
             .WithOption(ConfigOptions.DisableLogFile, true);
 
