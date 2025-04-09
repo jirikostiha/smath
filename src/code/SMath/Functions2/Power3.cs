a73d825ed71baa011a91f8e3f7ae5cc2e380f717
Jiri Kostiha <ijkdata@gmail.com>
2025-04-09 11:09:12 +0200

    feat: Power3

---
+++ src/code/SMath/Functions2/Power3.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+
+namespace SMath.Functions2;
+
+/// <summary>
+/// Power 3 function.
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Exponentiation#Power_functions">Wikipedia</a>
+/// </remarks>
+public class Power3 //: IMathFunction
+{
+    /// <inheritdoc />
+    public static string PlainTextFormula
+        => "x^3";
+
+    /// <inheritdoc />
+    public static N Eval<N>(N x)
+        where N : IMultiplyOperators<N, N, N>
+        => x * x * x;
+}
