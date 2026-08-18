0ed17325846a6270f62e0b0286668ac7d9fb5622
Jiri Kostiha <ijkdata@gmail.com>
2026-08-18 15:27:34 +0200

    feat(statistics): implement remaining means, median, mode, moments, skewness, and kurtosis

--- src/code/SMath/Statistics/mean/GeneralizedMean.cs
+++ src/code/SMath/Statistics/mean/GeneralizedMean.cs
@@ -1,18 +1,57 @@
-﻿namespace SMath.Statistics
-{
-    using System.Collections.Generic;
+using System.Numerics;
 
-    /// <summary>
-    /// Generalized mean or power mean or Hölder mean.
-    /// </summary>
-    /// <remarks>
-    /// <a href="https://en.wikipedia.org/wiki/Generalized_mean">wikipedia</a>
-    /// </remarks>
-    public static class GeneralizedMean
+namespace SMath.Statistics;
+
+/// <summary>
+/// Generalized mean or power mean or Hölder mean.
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Generalized_mean">Wikipedia</a>
+/// </remarks>
+public static class GeneralizedMean
+{
+    public static double Eval<N>(IEnumerable<N> sequence, double p)
+        where N : INumberBase<N>
     {
-        public static double f(IEnumerable<double> sequence)
+        if (p == 0) return GeometricMean.Eval(sequence);
+        if (p == 1) return ArithmeticMean.Eval(sequence);
+        if (p == 2) return QuadraticMean.Eval(sequence);
+        if (p == -1) return HarmonicMean.Eval(sequence);
+        if (p == 3) return CubicMean.Eval(sequence);
+
+        double sumPow = 0;
+        int count = 0;
+
+        foreach (var val in sequence)
         {
-            return default;
+            var d = double.CreateChecked(val);
+            sumPow += double.Pow(d, p);
+            count++;
         }
+
+        if (count == 0) return double.NaN;
+
+        return double.Pow(sumPow / count, 1d / p);
+    }
+
+    public static double Eval<N>(ReadOnlySpan<N> sequence, double p)
+        where N : INumberBase<N>
+    {
+        if (sequence.Length == 0) return double.NaN;
+        if (p == 0) return GeometricMean.Eval(sequence);
+        if (p == 1) return ArithmeticMean.Eval(sequence);
+        if (p == 2) return QuadraticMean.Eval(sequence);
+        if (p == -1) return HarmonicMean.Eval(sequence);
+        if (p == 3) return CubicMean.Eval(sequence);
+
+        double sumPow = 0;
+
+        for (int i = 0; i < sequence.Length; i++)
+        {
+            var d = double.CreateChecked(sequence[i]);
+            sumPow += double.Pow(d, p);
+        }
+
+        return double.Pow(sumPow / sequence.Length, 1d / p);
     }
 }
--- src/code/SMath/Statistics/mean/GeometricMean.cs
+++ src/code/SMath/Statistics/mean/GeometricMean.cs
@@ -1,21 +1,55 @@
-﻿namespace SMath.Statistics
+using System.Numerics;
+
+namespace SMath.Statistics;
+
+/// <summary>
+/// Geometric mean.
+/// </summary>
+/// <remarks>
+/// <a href="https://en.wikipedia.org/wiki/Geometric_mean">Wikipedia</a>
+/// </remarks>
+public static class GeometricMean
 {
-    using System;
-    using System.Collections.Generic;
-
-    /// <summary>
-    /// Geometric mean
-    /// </summary>
-    /// <remarks>
-    /// <a href="https://en.wikipedia.org/wiki/Geometric_mean">wikipedia</a>
-    /// </remarks>
-    public static class GeometricMean
+    public static double Eval<N>(IEnumerable<N> sequence)
+        where N : INumberBase<N>
     {
-        public static double f(IEnumerable<double> sequence)
-        {
-            var product = Product.Eval(sequence, out long count);
+        double sumLog = 0;
+        int count = 0;
+        bool hasZero = false;
 
-            return Math.Pow(product, 1 / (double)count);
+        foreach (var val in sequence)
+        {
+            var d = double.CreateChecked(val);
+            if (d < 0) return double.NaN;
+            if (d == 0) hasZero = true;
+            else sumLog += double.Log(d);
+            count++;
         }
+
+        if (count == 0) return double.NaN;
+        if (hasZero) return 0d;
+
+        return double.Exp(sumLog / count);
+    }
+
+    public static double Eval<N>(ReadOnlySpan<N> sequence)
+        where N : INumberBase<N>
+    {
+        if (sequence.Length == 0) return double.NaN;
+
+        double sumLog = 0;
+        bool hasZero = false;
+
+        for (int i = 0; i < sequence.Length; i++)
+        {
+            var d = double.CreateChecked(sequence[i]);
+            if (d < 0) return double.NaN;
+            if (d == 0) hasZero = true;
+            else sumLog += double.Log(d);
+        }
+
+        if (hasZero) return 0d;
+
+        return double.Exp(sumLog / sequence.Length);
     }
 }
