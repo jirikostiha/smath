fba46f7cfd153050e9c5b5978784c3a35efcc761
github-action-benchmark <github@users.noreply.github.com>
2026-08-24 16:40:12 +0000

    add Benchmark.Net (benchmarkdotnet) benchmark result for fd429f7fecb68db4af684d6d7bb2c0878a00180b

---
+++ dev/bench/data.js
@@ -0,0 +1,68 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1787589612725,
+  "repoUrl": "https://github.com/jirikostiha/smath",
+  "entries": {
+    "Benchmark.Net": [
+      {
+        "commit": {
+          "author": {
+            "email": "ijkdata@gmail.com",
+            "name": "Jiri Kostiha",
+            "username": "jirikostiha"
+          },
+          "committer": {
+            "email": "ijkdata@gmail.com",
+            "name": "Jiri Kostiha",
+            "username": "jirikostiha"
+          },
+          "distinct": true,
+          "id": "fd429f7fecb68db4af684d6d7bb2c0878a00180b",
+          "message": "docs: add documentation website badge, section, and PackageProjectUrl",
+          "timestamp": "2026-08-24T18:11:25+02:00",
+          "tree_id": "9d0576f6c53d0de0a17f9efbd6379d13339dfd1b",
+          "url": "https://github.com/jirikostiha/smath/commit/fd429f7fecb68db4af684d6d7bb2c0878a00180b"
+        },
+        "date": 1787589611997,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "SMath.Benchmarks.BinaryIntegerExtension_Benchmark.HammingDistance_Int",
+            "value": 6257.165664672852,
+            "unit": "ns",
+            "range": "± 16.84804664428306"
+          },
+          {
+            "name": "SMath.Benchmarks.BinaryIntegerExtension_Benchmark.HammingDistance_Long",
+            "value": 12478.296371459961,
+            "unit": "ns",
+            "range": "± 10.795122727395261"
+          },
+          {
+            "name": "SMath.Benchmarks.BinaryIntegerExtension_Benchmark.GCD_Int",
+            "value": 87122.00856526692,
+            "unit": "ns",
+            "range": "± 43.42762750029835"
+          },
+          {
+            "name": "SMath.Benchmarks.BinaryIntegerExtension_Benchmark.Pow_Int",
+            "value": 2586.34619140625,
+            "unit": "ns",
+            "range": "± 2.267933022169827"
+          },
+          {
+            "name": "SMath.Benchmarks.BinaryIntegerExtension_Benchmark.ToGrayCode",
+            "value": 3123.3520164489746,
+            "unit": "ns",
+            "range": "± 1.5535970684113476"
+          },
+          {
+            "name": "SMath.Benchmarks.BinaryIntegerExtension_Benchmark.FromGrayCode",
+            "value": 41305.09359741211,
+            "unit": "ns",
+            "range": "± 123.79549041718055"
+          }
+        ]
+      }
+    ]
+  }
+}
