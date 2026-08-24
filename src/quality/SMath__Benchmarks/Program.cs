a26802f847f1b705926ce85b3b44c80ad1289cbb
Jiri Kostiha <ijkdata@gmail.com>
2026-08-24 17:39:02 +0200

    refactor(benchmark): simplify Program.cs and rely on standard BenchmarkSwitcher

--- src/quality/SMath__Benchmarks/Program.cs
+++ src/quality/SMath__Benchmarks/Program.cs
@@ -1,30 +1,9 @@
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Exporters.Json;
 using BenchmarkDotNet.Running;
-using System.Runtime.CompilerServices;
 
 namespace SMath;
 
 public static class Program
 {
-    private static string GetSourceFilePathName([CallerFilePath] string? callerFilePath = null) => callerFilePath ?? string.Empty;
-
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("directory: " + Directory.GetCurrentDirectory());
-
-        if (!Directory.GetCurrentDirectory().EndsWith(typeof(Program).Assembly.GetName().Name ?? string.Empty, StringComparison.Ordinal))
-        {
-            //HACK: workaround for executing from VS because the output path of binaries is in different folder than usual
-            Directory.SetCurrentDirectory(Path.GetDirectoryName(GetSourceFilePathName()) ?? string.Empty);
-            Console.WriteLine("changed to: " + Directory.GetCurrentDirectory());
-        }
-
-        ManualConfig config = ManualConfig.Create(DefaultConfig.Instance.WithArtifactsPath("./../../../benchmarks"))
-            .AddExporter(JsonExporter.FullCompressed)
-            .WithOption(ConfigOptions.JoinSummary, true)
-            .WithOption(ConfigOptions.DisableLogFile, true);
-
-        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, config);
-    }
+    public static void Main(string[] args) =>
+        BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
 }
